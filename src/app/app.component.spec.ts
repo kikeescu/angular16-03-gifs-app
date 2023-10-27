4b0085f578f2dab539d5dd7dafa468282fcec5b9
kike <kike@pop-os.localdomain>
2023-10-27 15:17:06 +0200

    total

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'skeleton'`, () => {
+  it(`should have as title 'GifsApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('skeleton');
+    expect(app.title).toEqual('GifsApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('skeleton app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('GifsApp app is running!');
   });
 });
